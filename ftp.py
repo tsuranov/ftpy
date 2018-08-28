9704114b083a4684ca043a0f911a44c5f3db794d
asdV <tsuranov@yandex.ru>
2018-08-28 18:43:14 +0500

    multy threads

--- ftp.py
+++ ftp.py
@@ -3,54 +3,72 @@ import ftplib
 import os
 import asyncio
 import aiofiles
+import aioftp
 # 
 
 urlFTP = 'mirror.yandex.ru'
 pathFTP = 'knoppix/DVD'
-#
+filenames = []
+
 print ("Start FTP downloader!")
-#
-ftp = ftplib.FTP( urlFTP )
-ftp.login()
-ftp.cwd( pathFTP )
+#####################################################################
+#Однопоточный вариант
+#####################################################################
+#ftp = ftplib.FTP( urlFTP )
+#ftp.login()
+#ftp.cwd( pathFTP )
 
 #Получаем список файлов
-filenames = []
-def handleDownload (strFileName):
-    #отсекаем каталоги
-    if strFileName[0] != 'd':
-        #оставляем только имя файла из:
-        #-rw-r--r--    1 ftp      ftp            65 Aug 17 11:26 orel-2.12-14.08.2018_15.12.md5
-        filenames.append(strFileName[strFileName.rfind(" ") + 1 : ])
+#filenames = []
+#def handleDownload (strFileName):
+#    print (strFileName)
+#    #отсекаем каталоги
+#    if strFileName[0] == '-':
+#        #оставляем только имя файла из:
+#        #-rw-r--r--    1 ftp      ftp            65 Aug 17 11:26 orel-2.12-14.08.2018_15.12.md5
+#        filenames.append(strFileName[strFileName.rfind(" ") + 1 : ])
+#ftp.retrlines('LIST', handleDownload)
+#print("Count files: ", len(filenames) )
+#ftp.quit()
 
-ftp.retrlines('LIST', handleDownload)
-print("Count files: ", len(filenames) )
-ftp.quit()
+#####################################################################
+#Многопоточность
+#####################################################################
+
+async def get_list(urlFTP, pathFTP, filenames):
+    async with aioftp.ClientSession( urlFTP ) as client:
+        await client.change_directory( pathFTP )
+        for path, info in (await client.list(recursive=False)):
+            if info["type"] == "file":
+                filenames.append( path.name )
+
+loop = asyncio.get_event_loop()
+task = [asyncio.ensure_future(get_list(urlFTP, pathFTP, filenames) ), ]
+loop.run_until_complete(asyncio.wait( task ))
 
 #Асихронное закачка
-async def download_ftp(strFileName):
-    print("Download: ", strFileName)
-    ftp = ftplib.FTP( urlFTP )
-    ftp.login()
-    ftp.cwd( pathFTP )
+async def download_ftp(urlFTP, pathFTP, listFileName):
+    async with aioftp.ClientSession( urlFTP ) as client:
+        for strFileName in listFileName:
+            if await client.is_file(pathFTP + '/' + strFileName):
+                print ("Download: " + pathFTP + '/' + strFileName)
+                await client.download(pathFTP + '/' + strFileName)
+                print("End download: " + pathFTP + '/' + strFileName)
 
-    #не работает асихронная закачка или запись???
+#Разбитие на потоки
+def chunks(lst, chunk_size):
+    return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
 
-    #with open(strFileName, 'wb') as local_file:
-    #        ftp.retrbinary('RETR ' + strFileName, local_file.write)
-    
-    #async with aiofiles.open(strFileName, mode='wb') as local_file:
-    #    ftp.retrbinary('RETR ' + strFileName,  await local_file.write())
-    print("End download: ", strFileName)
-    ftp.quit()
+del filenames[0]
+lst_flnms = chunks(filenames, (len(filenames)//2 + 1) )
 
-    
-loop = asyncio.get_event_loop()
-tasks = [asyncio.ensure_future( download_ftp( filenames[i] )) for i in range(0, len(filenames))]
-async_download = asyncio.wait(tasks)
-loop.run_until_complete(async_download)
+async def async_download():
+    tasks = [asyncio.ensure_future(
+        download_ftp( urlFTP, pathFTP, lst_flnms[i] )) for i in range(0, 2)]
+    await asyncio.wait(tasks)
+
+loop.run_until_complete(async_download())
 loop.close()
-#ftp.quit()
 print ("End FTP downloader!")
 
 
