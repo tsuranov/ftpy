ace38dd796f7c4b9eae40cfae08be4a97ea6ee99
asdV <tsuranov@yandex.ru>
2018-08-30 18:56:21 +0500

    multyprocessing

--- ftp.py
+++ ftp.py
@@ -1,40 +1,72 @@
 #!/usr/bin/env python3
 import ftplib
 import os
+import time
+#асихронность
 import asyncio
-import aiofiles
 import aioftp
-# 
+#import aiofiles
+
+#многопоточноcть 
+#import threading
+import multiprocessing
+
 
 urlFTP = 'mirror.yandex.ru'
 pathFTP = 'knoppix/DVD'
 filenames = []
 
-print ("Start FTP downloader!")
+
 #####################################################################
 #Однопоточный вариант
 #####################################################################
-#ftp = ftplib.FTP( urlFTP )
-#ftp.login()
-#ftp.cwd( pathFTP )
+
+#####################################################################
+#MultyThread вариант
+#MultyProcessing вариант
+#####################################################################
+print ("Start FTP downloader!")
+ftp = ftplib.FTP( urlFTP )
+ftp.login()
+ftp.cwd( pathFTP )
 
 #Получаем список файлов
-#filenames = []
-#def handleDownload (strFileName):
-#    print (strFileName)
-#    #отсекаем каталоги
-#    if strFileName[0] == '-':
-#        #оставляем только имя файла из:
-#        #-rw-r--r--    1 ftp      ftp            65 Aug 17 11:26 orel-2.12-14.08.2018_15.12.md5
-#        filenames.append(strFileName[strFileName.rfind(" ") + 1 : ])
-#ftp.retrlines('LIST', handleDownload)
-#print("Count files: ", len(filenames) )
-#ftp.quit()
+filenames = []
+def handleDownload (strFileName):
+    print (strFileName)
+    #отсекаем каталоги
+    if strFileName[0] == '-':
+        #оставляем только имя файла из:
+        #-rw-r--r--    1 ftp      ftp            65 Aug 17 11:26 orel-2.12-14.08.2018_15.12.md5
+        filenames.append(strFileName[strFileName.rfind(" ") + 1 : ])
+ftp.retrlines('LIST', handleDownload)
+print("Count files: ", len(filenames) )
+ftp.quit()
+
+print ("Start FTP downloader!")
+def worker(strFileName):
+    ftp = ftplib.FTP( urlFTP )
+    ftp.login()
+    ftp.cwd( pathFTP )
+    print ("Download: " + pathFTP + '/' + strFileName)
+    with open(strFileName, 'wb') as f:
+        ftp.retrbinary('RETR ' + strFileName, f.write)
+    ftp.quit()
+
+for i in range(2):
+    #t = threading.Thread(target=worker, args=(filenames[i],))
+    t = multiprocessing.Process(target=worker, args=(filenames[i],))
+    t.start()
+  #  t.join()
+
+print("All Threads are queued, let's see when they finish!")
+
+time.sleep(600)
 
 #####################################################################
-#Многопоточность
+#Асихронность с непонятной многопоточностью MainThread + 21 Thread
 #####################################################################
-
+print ("Start FTP async downloader!")
 async def get_list(urlFTP, pathFTP, filenames):
     async with aioftp.ClientSession( urlFTP ) as client:
         await client.change_directory( pathFTP )
@@ -55,13 +87,13 @@ async def download_ftp(urlFTP, pathFTP, listFileName):
                 await client.download(pathFTP + '/' + strFileName)
                 print("End download: " + pathFTP + '/' + strFileName)
 
-#Разбитие на потоки
+#Разбитие список на список со списками
 def chunks(lst, chunk_size):
     return [lst[i:i+chunk_size] for i in range(0, len(lst), chunk_size)]
 
+#Асихронная обработка двух списоков
 del filenames[0]
 lst_flnms = chunks(filenames, (len(filenames)//2 + 1) )
-
 async def async_download():
     tasks = [asyncio.ensure_future(
         download_ftp( urlFTP, pathFTP, lst_flnms[i] )) for i in range(0, 2)]
